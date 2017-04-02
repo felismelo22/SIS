ad4be174f1b99ae103416bf08c2b08e984e37821
iwan safrudin <iwan@fisip.net>
2017-04-03 06:12:55 +0700

    mastering

---
+++ application/views/admin/footer.php
@@ -0,0 +1,10 @@
+<!-- jQuery -->
+<script src="<?php echo base_url().'templates/admin/'; ?>js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo base_url().'templates/admin/'; ?>js/bootstrap.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="<?php echo base_url().'templates/admin/'; ?>js/plugins/morris/raphael.min.js"></script>
+<script src="<?php echo base_url().'templates/admin/'; ?>js/plugins/morris/morris.min.js"></script>
+<script src="<?php echo base_url().'templates/admin/'; ?>js/plugins/morris/morris-data.js"></script>
---
+++ application/views/admin/sidebar_menu.php
@@ -0,0 +1,16 @@
+<ul class="nav navbar-nav side-nav">
+	<li class="active">
+		<a href="#"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+	</li>
+	<li>
+		<a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-user"></i> User <i class="fa fa-fw fa-caret-down"></i></a>
+		<ul id="demo" class="collapse">
+			<li>
+				<a href="#">User List</a>
+			</li>
+			<li>
+				<a href="#">User Add</a>
+			</li>
+		</ul>
+	</li>
+</ul>
